b6f28b0daf4f4ba363e6d9ddc608f41e50732932
wanyamae <anuelyama3@gmail.com>
2025-07-03 21:50:01 +0300

    Updated subquerries

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -12,19 +12,3 @@ SELECT "User".*, Booking.*
 FROM "User"
 FULL OUTER JOIN Booking ON "User".user_id = Booking.user_id
 
-SELECT property_id, name
-FROM Property
-WHERE property_id IN (
-    SELECT property_id
-    FROM Review
-    GROUP BY property_id
-    HAVING AVG(rating) > 4.0
-);
-
-SELECT user_id, first_name, last_name
-FROM "User" u
-WHERE (
-    SELECT COUNT(*)
-    FROM Booking b
-    WHERE b.user_id = u.user_id
-) > 3;
