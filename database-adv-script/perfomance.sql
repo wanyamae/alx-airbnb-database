f978a58e12d2fd9d22820053e2a15d5147f8a1ca
wanyamae <anuelyama3@gmail.com>
2025-07-03 22:13:55 +0300

    .

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -9,8 +9,9 @@ SELECT
 FROM Booking
 INNER JOIN "User" ON Booking.user_id = "User".user_id
 INNER JOIN Property ON Booking.property_id = Property.property_id
-LEFT JOIN Payment ON Booking.booking_id = Payment.booking_id;
-
+LEFT JOIN Payment ON Booking.booking_id = Payment.booking_id
+WHERE Booking.status = 'confirmed'
+  AND Property.location = 'New York';
 
 EXPLAIN ANALYZE
 SELECT 
